0b06d9bc1a392c33e7ff1d4895aa912f58dc7d2c
dimon <dimagol64@gmail.com>
2020-04-22 21:36:24 +0300

    auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,35 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Button from "../../components/UI/Button/Button";
+import css from './Auth.module.scss';
 export default class Auth extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Auth</h2>
-            </div>
-        )
+    loginHandler = () => {
+
     }
+
+    registerHandler = () => {
+
+    }
+
+    sumbitHandler = (e) => {
+        e.preventDefault();
+    }
+
+  render() {
+    return (
+      <div className={css.Auth}>
+        <h2>Auth</h2>
+        <form onSubmit={this.sumbitHandler} className={css.AuthForm}>
+          <input type="text" />
+          <input type="text" />
+          <Button type="success" onClic={this.loginHandler}>
+            Enter
+          </Button>
+
+          <Button type="primary" onClic={this.registerHandler}>
+            Signup
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
