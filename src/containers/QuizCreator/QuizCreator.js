b8e8e4c825b3eb2d111b4934e59af7808331d2f7
dimon <dimagol64@gmail.com>
2020-04-23 10:41:56 +0300

    quiz create

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import css from "./QuizCreator.module.scss";
+import Button from "../../components/UI/Button/Button";
+import Input from "../../components/UI/Input/Input";
 
 export default class QuizCreator extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={css.QuizCreator}>
+        <div>
+          <h1>ddd</h1>
+          <form onSubmit={this.submitHandler}>
+            <Input type='text'/>
+            <hr/>
+            <Input type='text'/>
+            <Input type='text'/>
+            <Input type='text'/>
+            <Input type='text'/>
+            <Button type="primary" onClick={this.addQuestionHandler}>
+              add question
+            </Button>
+            <Button type="success" onClick={this.createQuizHandler}>
+              Create test
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
